5560b3a1fdf195bc6eedb190cfa41ef748f2a88e
patryk920 <pblonkowski@wp.pl>
2019-06-11 22:41:42 +0200

    zmiana

--- src/sudoku.java
+++ src/sudoku.java
@@ -3,37 +3,37 @@
  */
 public class sudoku {
     public static void main(String[] args) {
-        int [] [] sudoku = new int[9][9];
-        sudoku[0][0] = 2;
-        sudoku[0][2] = 6;
-        sudoku[0][7] = 8;
-        sudoku[1][3] = 5;
-        sudoku[1][4] = 7;
-        sudoku[1][7] = 4;
-        sudoku[2][2] = 7;
-        sudoku[2][6] = 3;
-        sudoku[2][8] = 5;
-        sudoku[3][0] = 6;
-        sudoku[3][3] = 7;
-        sudoku[3][5] = 1;
-        sudoku[3][6] = 5;
-        sudoku[3][8] = 2;
-        sudoku[4][3] = 3;
-        sudoku[4][5] = 8;
-        sudoku[5][0] = 7;
-        sudoku[5][2] = 1;
-        sudoku[5][3] = 2;
-        sudoku[5][5] = 9;
-        sudoku[5][8] = 6;
-        sudoku[6][0] = 5;
-        sudoku[6][2] = 3;
-        sudoku[6][6] = 6;
-        sudoku[7][1] = 9;
-        sudoku[7][4] = 2;
-        sudoku[7][2] = 7;
-        sudoku[8][1] = 6;
-        sudoku[8][6] = 4;
-        sudoku[8][8] = 8;
+        int[][] tab = new int[9][9];
+        tab[0][0] = 2;
+        tab[0][2] = 6;
+        tab[0][7] = 8;
+        tab[1][3] = 5;
+        tab[1][4] = 7;
+        tab[1][7] = 4;
+        tab[2][2] = 7;
+        tab[2][6] = 3;
+        tab[2][8] = 5;
+        tab[3][0] = 6;
+        tab[3][3] = 7;
+        tab[3][5] = 1;
+        tab[3][6] = 5;
+        tab[3][8] = 2;
+        tab[4][3] = 3;
+        tab[4][5] = 8;
+        tab[5][0] = 7;
+        tab[5][2] = 1;
+        tab[5][3] = 2;
+        tab[5][5] = 9;
+        tab[5][8] = 6;
+        tab[6][0] = 5;
+        tab[6][2] = 3;
+        tab[6][6] = 6;
+        tab[7][1] = 9;
+        tab[7][4] = 2;
+        tab[7][2] = 7;
+        tab[8][1] = 6;
+        tab[8][6] = 4;
+        tab[8][8] = 8;
 
     }
 }
